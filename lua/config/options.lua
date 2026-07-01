1995ddf5a827eebb15729a24f33ef88d20941d4b
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-01 17:12:51 -0400

    feat: tabs instead of spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local tab_width = 4
+
+vim.opt.expandtab = false -- Use tabs instead of spaces
+vim.opt.tabstop = tab_width -- Number of spaces tabs count for
+vim.opt.shiftwidth = tab_width -- Size of an indent
+vim.opt.softtabstop = tab_width -- Number of spaces tabs count for while editing
