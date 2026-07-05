6c65f63523d164876f44eb5018449c09324bd6e2
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-05 18:14:42 -0400

    feat: word wrap with indent

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,8 @@ vim.opt.expandtab = false -- Use tabs instead of spaces
 vim.opt.tabstop = tab_width -- Number of spaces tabs count for
 vim.opt.shiftwidth = tab_width -- Size of an indent
 vim.opt.softtabstop = tab_width -- Number of spaces tabs count for while editing
+
+vim.opt.wrap = true -- Wrap long lines
+vim.opt.linebreak = true -- Wrap at word boundaries, not mid-word
+vim.opt.breakindent = true -- Wrapped lines continue at the same indent (VS Code style)
+vim.opt.breakindentopt = "shift:" .. tab_width -- Indent wrapped lines one extra level, like VS Code's wrappingIndent: "indent"
