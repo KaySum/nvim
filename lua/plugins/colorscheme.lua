ece4e7d107b04169ce517bb640f8a2e442fbc260
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-12 19:51:55 -0400

    feat: vscode theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+}
