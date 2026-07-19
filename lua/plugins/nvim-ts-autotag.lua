7c9fa29bb57f6dbeb368ad9f250b6714f57173bb
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-19 19:01:39 -0400

    feat: add nvim-ts-autotag plugin

---
+++ lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "html",
+      "xml",
+      "svg",
+      "svelte",
+      "vue",
+      "astro",
+      "markdown",
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+    },
+    opts = {},
+  },
+}
