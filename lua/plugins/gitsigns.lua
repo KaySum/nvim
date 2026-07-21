91e27d0ecc6515110356c66d9f6d1e29a911d552
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-21 18:47:47 -0400

    fix(gitsigns): lower priority of virtual text, so it comes after "/" searches

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -5,6 +5,9 @@ return {
       current_line_blame = true,
       current_line_blame_opts = {
         delay = 300,
+        -- NOTE: This is so git blame renders after the `/` search count.
+        -- This is one above noice's default 4096 virtual text priority.
+        virt_text_priority = 4097,
       },
     },
   },
