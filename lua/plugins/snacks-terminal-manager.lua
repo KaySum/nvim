663e62094caf1efba782aca31f2953f2277b19bb
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-25 22:28:47 -0400

    feat: add snacks terminal manager plugin

---
+++ lua/plugins/snacks-terminal-manager.lua
@@ -0,0 +1,22 @@
+local keys = {
+  { "<c-/>", "<cmd>SnacksTerminalToggle<cr>", mode = { "n", "t" }, desc = "Terminal (Root Dir)" },
+  { "<c-_>", "<cmd>SnacksTerminalToggle<cr>", mode = { "n", "t" }, desc = "which_key_ignore" },
+  { "<leader>tt", "<cmd>SnacksTerminalPick<cr>", desc = "Switch Terminal" },
+  { "<leader>tr", "<cmd>SnacksTerminalRename<cr>", desc = "Rename Terminal" },
+  { "<leader>td", "<cmd>SnacksTerminalClose<cr>", desc = "Close Terminal" },
+}
+for i = 1, 9 do
+  keys[#keys + 1] = { "<leader>t" .. i, "<cmd>" .. i .. "SnacksTerminalToggle<cr>", desc = "Terminal #" .. i }
+end
+
+return {
+  "KaySum/nvim-snacks-terminal-manager",
+  dependencies = { "folke/snacks.nvim" },
+  lazy = false,
+  opts = {
+    root = function()
+      return LazyVim.root()
+    end,
+  },
+  keys = keys,
+}
