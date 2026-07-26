3956594b071875f7bc37d496255385558554f6fe
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-26 14:52:01 -0400

    feat: treesitter svelte injections plugin

---
+++ lua/plugins/svelte-postcss-treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "KaySum/svelte-postcss-treesitter.nvim",
+    ft = "svelte",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "css", "scss" } },
+  },
+}
