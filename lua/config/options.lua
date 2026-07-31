165153df970070b5534046fa944ee65a9b8e1fa5
KaySum <43190281+KaySum@users.noreply.github.com>
2026-07-30 22:52:42 -0400

    feat(vim sleuth): scale wrapped-line indent by detected shift width

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Factor applied to shiftwidth for the wrapped-line indent (VS Code deepIndent = 2).
+-- Global so plugins/vim-sleuth.lua can reuse it when it recomputes breakindentopt.
+vim.g.wrap_indent_multiplier = 2
+
 local tab_width = 4
 
 vim.opt.expandtab = false -- Use tabs instead of spaces
@@ -12,7 +16,7 @@ vim.opt.softtabstop = tab_width -- Number of spaces tabs count for while editing
 vim.opt.wrap = true -- Wrap long lines
 vim.opt.linebreak = true -- Wrap at word boundaries, not mid-word
 vim.opt.breakindent = true -- Wrapped lines continue at the same indent (VS Code style)
-vim.opt.breakindentopt = "shift:" .. 2 * tab_width -- Indent wrapped lines two extra levels, like VS Code's wrappingIndent: "deepIndent"
+vim.opt.breakindentopt = "shift:" .. vim.g.wrap_indent_multiplier * tab_width -- Indent wrapped lines like VS Code's wrappingIndent: "deepIndent"
 
 vim.opt.swapfile = false
 
--- lua/plugins/vim-sleuth.lua
+++ lua/plugins/vim-sleuth.lua
@@ -6,5 +6,19 @@ return {
       "BufNewFile", -- brand-new file -> set up detection for the buffer
       "BufFilePost", -- buffer renamed (:file/:saveas) -> re-detect (new name/extension/dir)
     },
+    -- Track the wrapped-line shift to sleuth's detected shiftwidth (deepIndent).
+    init = function()
+      vim.api.nvim_create_autocmd("OptionSet", {
+        pattern = "shiftwidth",
+        callback = function()
+          local shift_width = vim.v.option_new
+          -- shiftwidth=0 means "follow tabstop", so fall back to it
+          if shift_width == 0 then
+            shift_width = vim.bo.tabstop
+          end
+          vim.opt_local.breakindentopt = "shift:" .. vim.g.wrap_indent_multiplier * shift_width
+        end,
+      })
+    end,
   },
 }
