8cfa5856c802372d4601a8fa4070271424498c69
KaySum <43190281+KaySum@users.noreply.github.com>
2026-08-01 19:54:26 -0400

    fix(vim-sleuth): re-detect indentation after format-on-save

--- lua/plugins/vim-sleuth.lua
+++ lua/plugins/vim-sleuth.lua
@@ -8,7 +8,9 @@ return {
     },
     -- Track the wrapped-line shift to sleuth's detected shiftwidth (deepIndent).
     init = function()
+      local group = vim.api.nvim_create_augroup("sleuth_breakindent", { clear = true })
       vim.api.nvim_create_autocmd("OptionSet", {
+        group = group,
         pattern = "shiftwidth",
         callback = function()
           local shift_width = vim.v.option_new
@@ -17,6 +19,14 @@ return {
             shift_width = vim.bo.tabstop
           end
           vim.opt_local.breakindentopt = "shift:" .. vim.g.wrap_indent_multiplier * shift_width
+        end,
+      })
+      -- Format-on-save may reindent the buffer; re-detect so shiftwidth (and the
+      -- breakindentopt above) reflect the formatted file instead of the pre-save style.
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        group = group,
+        callback = function()
+          vim.cmd("silent! Sleuth")
         end,
       })
     end,
