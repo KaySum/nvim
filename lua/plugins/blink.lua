0c51898d437702fcb1305d537d6af0e81c3f2467
KaySum <43190281+KaySum@users.noreply.github.com>
2026-08-07 18:31:44 -0400

    feat(blink): disable snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -14,5 +14,18 @@ return {
     enabled = function()
       return not vim.tbl_contains(disabled_filetypes, vim.bo.filetype)
     end,
+    sources = {
+      providers = {
+        snippets = { enabled = false },
+        lsp = {
+          transform_items = function(_, items)
+            local snippet = require("blink.cmp.types").CompletionItemKind.Snippet
+            return vim.tbl_filter(function(item)
+              return item.kind ~= snippet
+            end, items)
+          end,
+        },
+      },
+    },
   },
 }
