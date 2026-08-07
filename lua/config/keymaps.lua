35701dc62a2eafccb930a9ea0178e63cb6136930
KaySum <43190281+KaySum@users.noreply.github.com>
2026-08-07 19:16:01 -0400

    refactor(keymaps): removed unused/redundant keymaps and helpers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,22 +5,4 @@
 vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz", { desc = "Half-page down and center cursor" })
 vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz", { desc = "Half-page up and center cursor" })
 
-local function ctrl(keys)
-  -- Cmd on macOS, Ctrl on Windows/Linux
-  local mod = vim.fn.has("mac") == 1 and "D" or "C"
-  return "<" .. mod .. "-" .. keys .. ">"
-end
-
-vim.keymap.set({ "n", "i", "v" }, ctrl("s"), "<cmd>w<cr>", { desc = "Save file" })
-
-vim.keymap.set("n", ctrl("z"), "u", { desc = "Undo" })
-vim.keymap.set("i", ctrl("z"), "<C-o>u", { desc = "Undo" })
-vim.keymap.set("n", ctrl("S-z"), "<C-r>", { desc = "Redo" })
-vim.keymap.set("i", ctrl("S-z"), "<C-o><C-r>", { desc = "Redo" })
-
-vim.keymap.set("i", ctrl("v"), "<C-r>+", { desc = "Paste" })
-
--- Toggle comment
--- vim.keymap.set("n", ctrl("/"), "gcc", { desc = "Toggle Comment", remap = true })
--- vim.keymap.set("v", ctrl("/"), "gc", { desc = "Toggle Comment", remap = true })
-vim.keymap.set("i", ctrl("/"), "<Cmd>normal gcc<CR>", { desc = "Toggle Comment" })
+vim.keymap.set({ "n", "i", "v" }, "<D-s>", "<cmd>w<cr>", { desc = "Save file" })
